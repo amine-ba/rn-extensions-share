48715b2206b84073b2dff5b040fc52d352506a90
Amine Ballalou <49789676+amine-ba@users.noreply.github.com>
2021-04-19 08:12:45 +0100

    Update ReactNativeShareExtension.h

--- ios/ReactNativeShareExtension.h
+++ ios/ReactNativeShareExtension.h
@@ -1,5 +1,6 @@
 #import <UIKit/UIKit.h>
 #import "React/RCTBridgeModule.h"
+#import <UMCore/UMAppDelegateWrapper.h>
 
 @interface ReactNativeShareExtension : UIViewController<RCTBridgeModule>
 - (UIView*) shareView;
